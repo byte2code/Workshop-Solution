552c06d59af039116b18c09f92d60f62d212949a
Mausam Giri <48901104+byte2code@users.noreply.github.com>
2022-10-13 12:01:13 +0530

    created w6p1.c

---
+++ Workshop 6/w6p1.c
@@ -0,0 +1,35 @@
+/*
+*****************************************************************************
+                          Workshop - #6 (P1)
+Full Name  : 
+Student ID#: 
+Email      : 
+Section    : 
+
+Authenticity Declaration:
+I declare this submission is the result of my own work and has not been
+shared with any other student or 3rd party content provider. This submitted
+piece of work is entirely of my own creation.
+*****************************************************************************
+*/
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main(void)
+{
+
+    return 0;
+}
+
+/*   PDF Provided Formatting:
+
+Step #7 (a):
+    printf("Item Priority Financed        Cost\n");
+    printf("---- -------- -------- -----------\n");
+
+Step #7 (b):
+    printf("%3d  %5d    %5c    %11.2lf\n", i + 1, itemPriority[i], itemCanFinance[i], itemCost[i]);
+
+*/
